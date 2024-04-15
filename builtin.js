5c7b2f41ffa8eb75a76860e04245416faf00f1dc
Jared Lambert <jaredclambert@gmail.com>
2024-04-15 10:41:46 -0400

    loops through pages and doesn't get caught

--- builtin.js
+++ builtin.js
@@ -22,36 +22,56 @@ import puppeteer from "puppeteer";
     }
     return results;
   });
-  // click on each company
-  for (var item of links) {
-    await page.goto(item);
-    const selector =
-      'button.job-category-pill[data-testid="job-filter-job-pill-6"]';
-    const isDisabled = await page
-      .locator(selector)
-      .map((button) => button.disabled)
-      .wait();
+  var currentPage = 1;
+  var finished = false;
+  while (!finished) {
+    // click on each company
+    for (var item of links) {
+      await page.goto(item);
+      const selector =
+        'button.job-category-pill[data-testid="job-filter-job-pill-6"]';
+      const isDisabled = await page
+        .locator(selector)
+        .map((button) => button.disabled)
+        .wait();
 
-    // wait for a random amount of time to fool the bot detectors
-    const randomWaitTime = Math.floor(Math.random() * 1000);
-    await delay(randomWaitTime);
-    // scroll a bit to fool them
-    await page.evaluate(() => {
-      window.scrollTo(0, 500);
-    });
-    await delay(randomWaitTime);
-    if (isDisabled) {
-      continue;
+      // wait for a random amount of time to fool the bot detectors
+      const randomWaitTime = Math.floor(Math.random() * 1000) + 2000;
+      await delay(randomWaitTime);
+      // scroll a bit to fool them
+      await page.evaluate(() => {
+        window.scrollTo(0, 500);
+      });
+      await delay(randomWaitTime);
+      if (isDisabled) {
+        await goBack(page);
+      }
+
+      await page
+        .locator(selector)
+        .filter((button) => !button.disabled)
+        .click();
+      await delay(randomWaitTime);
+      const jobs = page.locator(".jobs row .job-item");
+
+      // go back to prev page
+      await goBack(page);
+      await delay(randomWaitTime);
     }
 
-    await page
-      .locator(selector)
-      .filter((button) => !button.disabled)
-      .click();
-    const jobs = page.locator(".jobs row .job-item");
+    // click on the next page at the bottom, if it exists
+    currentPage += 1;
+    const selector = `li.page-item ::-p-text(${currentPage})`;
+
+    const nextButton = await page.locator(selector).wait();
+    if (nextButton) {
+      await page.click(selector);
+    } else {
+      finished = true;
+    }
   }
 
-  //await browser.close();
+  await browser.close();
 })();
 
 function delay(time) {
@@ -59,3 +79,10 @@ function delay(time) {
     setTimeout(resolve, time);
   });
 }
+
+function goBack(page) {
+  return page.evaluate(() => {
+    window.history.back();
+  });
+}
+
